a606c4d3249a729ca3b1523a3ce3843ac20ab99f
Abdul Rehman <asmaabdulrehman@example.com>
2026-08-30 00:13:28 +0500

    use env variable for api url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,15 +1,7 @@
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:5000/api', 
-//   headers: { 'Content-Type': 'application/json' },
-// });
-
-// export default axiosInstance;
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'http://localhost:5000/api', 
   headers: { 'Content-Type': 'application/json' },
 });
 
