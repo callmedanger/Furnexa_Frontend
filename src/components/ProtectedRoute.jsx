919df4c8ef8839381690c8e5693d6d3b86581b68
Abdul Rehman <asmaabdulrehman@example.com>
2026-09-05 19:35:11 +0500

    w

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,9 +4,10 @@ import { AUTH_STORAGE_KEY } from '../pages/Login';
 const ProtectedRoute = () => {
   const location = useLocation();
 
-  if (sessionStorage.getItem(AUTH_STORAGE_KEY) !== 'true') {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
+  // Temporary development bypass: restore this block to re-enable login protection.
+  // if (sessionStorage.getItem(AUTH_STORAGE_KEY) !== 'true') {
+  //   return <Navigate to="/login" replace state={{ from: location }} />;
+  // }
 
   return <Outlet />;
 };
